b34c20885af51a4dfab31f1f6ec41f16a3fd263e
Taras <tarasmaliarchuk@itjet.io>
2023-05-27 15:47:34 +0300

    feat: finish

--- app/src/db/database.providers.ts
+++ app/src/db/database.providers.ts
@@ -1,17 +1,18 @@
 import { Sequelize } from 'sequelize-typescript';
-import {User} from "./models/user/user.entity";
-import {Quiz} from "./models/quiz/quiz.entity";
+import { User } from "./models/user/user.entity";
+import { Quiz } from "./models/quiz/quiz.entity";
+
 export const databaseProviders = [
     {
         provide: 'SEQUELIZE',
         useFactory: async () => {
             const sequelize = new Sequelize({
                 dialect: 'mysql',
-                host: 'localhost',
-                port: 3306,
-                username: 'user',
-                password: 'password',
-                database: 'db',
+                host: process.env.DATABASE_HOST || 'localhost',
+                port: parseInt(process.env.DATABASE_PORT) || 3306,
+                username: process.env.DATABASE_USERNAME || 'user',
+                password: process.env.DATABASE_PASSWORD || 'password',
+                database: process.env.DATABASE_NAME || 'db',
             });
             sequelize.addModels([User, Quiz]);
             await sequelize.sync();
@@ -19,3 +20,4 @@ export const databaseProviders = [
         },
     },
 ];
+
--- telegram-bot/src/index.js
+++ telegram-bot/src/index.js
@@ -11,8 +11,10 @@ const stage = new Scenes.Stage([quizScene]);
 const bot = new Telegraf(process.env.BOT_TOKEN);
 bot.use(session());
 bot.use(stage.middleware());
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 bot.start(async ctx => {
     const {first_name, username, id} = ctx.message.from;
+    await delay(5000);
     onStartCreateUser(ctx, {id, first_name, username}).
     then((_) => ctx.scene.enter(quizSceneId));
 
